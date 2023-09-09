7168715b492baf58d4df6acc37de402c0e15bb4f
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-09-09 19:58:29 -0400

    minor changes, test articles added for app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
         @articles = Article.all 
     end
     def show
-
+        @admin = @article.admin
     end
     def new
         @article = Article.new
