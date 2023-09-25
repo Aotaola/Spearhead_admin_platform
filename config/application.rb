288f70dd1497716e90a18a883284137048bd09d5
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-09-25 11:39:38 -0400

    updated articles columns

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module AfcMobileAdmin
     # in config/environments, which are processed later.
     #
     config.time_zone = 'Eastern Time (US & Canada)'
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
