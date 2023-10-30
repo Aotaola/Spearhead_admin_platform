e05aed0f8957dbf2f29994749cf52e47769d70b4
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-10-30 10:23:59 -0400

    invoices controller compleated

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -22,9 +22,20 @@ class InvoicesController < ApplicationController
     end
     def create
         @invoice = Invoice.new(invoice_params)
-        @invoice.patient_id = 
+        @invoice.patient_id = @patient.id
         if @invoice.save
-
+            redirect_to @patient, notice: "invoice was successfully created"
+         else
+            render :new
+            flash[:alert] = "there was an error creating the invoice"
+        end
+    end
+    def destroy
+        @invoice.destroy
+        if @invoice.deleted
+            flash[:notice] = "invoice was successfully deleted"
+        else
+            flash[:alert] = "there was an error deleting the invoice"
         end
     end
 
